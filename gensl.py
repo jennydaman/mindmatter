d0164f7b09fb3fd8d6937f3db1c2e22adf30381a
Jennings Zhang <jennydaman@users.noreply.github.com>
2017-06-04 21:25:50 -0400

    Basic directory structure and scripts. Add index.html

---
+++ gensl.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import sys
+
+baseurl = "http://jennydaman.github.io/systole"
+if len(sys.argv) > 1:
+    baseurl = sys.argv[1]
+
+subjectsDir = "./subjects/"
+
+"""Creates an array of dictionaries.
+Dictionaries have a subject name and an array of questions."""
+subjectStructure = [{'subjectStorageKey': subjectName, 'subjectName': subjectName, "questions": []}
+                    for subjectName in next(os.walk(subjectsDir))[1]]
+
+
+for subjectDict in subjectStructure:
+
+    sdir = "/" + subjectDict['subjectStorageKey']
+
+    titleFName = "." + sdir + "/metatitle"
+    if os.path.isfile(titleFName):
+        subjectDict['subjectName'] = open(
+            titleFName, 'r').read(32).replace("\n", "")
+
+    for questionFile in next(os.walk(subjectsDir + sdir))[2]:
+
+        if not questionFile.endswith(".json"):
+            continue
+        qdir = sdir + "/" + questionFile
+        subjectDict['questions'].append(
+            {"qpath": baseurl + qdir, "timeSE": os.path.getmtime(subjectsDir + qdir)})
+
+print(json.dumps(subjectStructure, sort_keys=True, indent=4))
