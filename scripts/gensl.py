aa237b0dbdacf841212c5337fe6bf055228c65ce
Jennings Zhang <jennydaman@users.noreply.github.com>
2017-08-11 15:55:20 -0400

    Total questions in subjectsDB.json

--- scripts/gensl.py
+++ scripts/gensl.py
@@ -12,11 +12,12 @@ if len(sys.argv) > 1:
 
 """Creates an array of dictionaries.
 Dictionaries have a subject name and an array of questions."""
-subjectStructure = [{'subjectStorageKey': subjectName, 'subjectName': subjectName, "questions": []}
-                    for subjectName in next(os.walk(subjectsDir))[1]]
+subjectStructure = {
+    'subjects': [{'subjectStorageKey': subjectName, 'subjectName': subjectName, "questions": []} for subjectName in next(os.walk(subjectsDir))[1]],
+    'totalQuestions': 0
+}
 
-
-for subjectDict in subjectStructure:
+for subjectDict in subjectStructure['subjects']:
 
     sdir = "/" + subjectDict['subjectStorageKey']
 
@@ -31,7 +32,7 @@ for subjectDict in subjectStructure:
         if not questionFile.endswith(".json"):
             continue
         qdir = sdir + "/" + questionFile
-        subjectDict['questions'].append(
-            {"qpath": baseurl + "/subjects" + qdir, "timeSE": os.path.getmtime(subjectsDir + qdir)})
+        subjectDict['questions'].append({"qpath": baseurl + "/subjects" + qdir, "timeSE": os.path.getmtime(subjectsDir + qdir)})
+        subjectStructure['totalQuestions'] += 1
 
 print(json.dumps(subjectStructure, sort_keys=True, indent=4))
