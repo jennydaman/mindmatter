f253e955c6889c9ffad3f8e91c2f359326baa9d1
Jennings Zhang <jenni_zh@protonmail.com>
2017-12-26 13:15:38 -0500

    Improve gensl.py, light error checking

--- scripts/gensl.py
+++ scripts/gensl.py
@@ -8,39 +8,47 @@ from time import gmtime
 
 baseurl = "https://jennydaman.github.io/mindmatter"
 
-subjectsDir = "subjects/"
+srootDir = "subjects"
 if len(sys.argv) > 1:
-    subjectsDir = sys.argv[1]
+    srootDir = sys.argv[1]
 
-"""Creates an array of dictionaries.
-Dictionaries have a subject name and an array of questions."""
 subjectStructure = {
-    'subjects': [{'subjectStorageKey': subjectName, 'subjectName': subjectName, "questions": []} for subjectName in next(os.walk(subjectsDir))[1]],
+    'subjects': [{'subjectFolder': subjectName, 'subjectName': subjectName, "questions": []} for subjectName in next(os.walk(srootDir))[1]],
     'totalQuestions': 0,
-    'updateTime': timegm(gmtime())
+    'gmtime': timegm(gmtime())
 }
 
-for subjectDict in subjectStructure['subjects']:
+for currentSubject in subjectStructure['subjects']:
 
-    sdir = "/" + subjectDict['subjectStorageKey']
-
-    titleFName = subjectsDir + sdir + "/title"
+    titleFName = srootDir + "/" + currentSubject['subjectFolder'] + "/title"
 
     if os.path.isfile(titleFName):
-        subjectDict['subjectName'] = open(
-            titleFName, 'r').read(32).replace("\n", "")
+        currentSubject['subjectName'] = open(titleFName, 'r').read(36).replace("\n", "")
 
-    for questionFile in next(os.walk(subjectsDir + sdir))[2]:
+    for questionFileName in next(os.walk(srootDir + "/" + currentSubject['subjectFolder']))[2]:
 
-        if not questionFile.endswith(".json"):
+        qdir = srootDir + "/" + currentSubject['subjectFolder'] + "/" + questionFileName
+        if not questionFileName.endswith(".json"):
+            print('W: [SKIP] ' + qdir, file=sys.stderr)
             continue
-        qdir = sdir + "/" + questionFile
-        subjectDict['questions'].append({"qpath": baseurl + "/subjects" + qdir, "timeSE": os.path.getmtime(subjectsDir + qdir)})
+
+        questionData = json.load(open(qdir))
+        if not 'question' in questionData:
+            print('E [Missing question] ' + qdir, file=sys.stderr)
+            sys.exit()
+        if not 'answer' in questionData:
+            print('E [Missing answer] ' + qdir, file=sys.stderr)
+            sys.exit()
+        if not 'type' in questionData or questionData['type'] not in ['blank']:
+            print('E [invalid question type] ' + qdir, file=sys.stderr)
+            sys.exit()
+
+        currentSubject['questions'].append({"qpath": baseurl + "/" + qdir, "mtime": os.path.getmtime(qdir)})
         subjectStructure['totalQuestions'] += 1
 
 
 for subject in subjectStructure["subjects"]:
-    subject["enabled"] = False
+    subject["enabled"] = True
     for question in subject["questions"]:
         question["chance"] = subjectStructure['totalQuestions']
 
